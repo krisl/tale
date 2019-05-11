3c1e0ba505d9cac415a2283917bafb92fa8f50d6
Aaron Lipinski <kris.lipinski@gmail.com>
2019-05-11 23:27:21 +1200

    le room

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Canvas from './canvas';
 import './App.css';
+
+const Room = () => { 
+  const [photos, setPhotos] = useState([])
+  return (
+    <div>
+      <Canvas peers={[]} photos={photos} socket={{send: sent => {
+        sent = JSON.parse(sent)
+        console.log({sent})
+        if (sent.type === 'ROOM/ADD_PHOTO') {
+          sent.payload._id = Math.random().toString(36).substr(2, 5)
+          console.log('adding', sent.payload)
+          const newphotos = [sent.payload, ...photos]
+          console.log({newphotos})
+          setPhotos(newphotos)
+        }
+      }}} />
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
+      <Room />
     </div>
   );
 }
