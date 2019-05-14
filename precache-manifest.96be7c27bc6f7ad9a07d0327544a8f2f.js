87f61913a48b22e7d2e8b32ddf99aed3118407d7
Aaron Lipinski <kris.lipinski@gmail.com>
2019-05-14 15:30:14 +1200

    Updates

---
+++ precache-manifest.96be7c27bc6f7ad9a07d0327544a8f2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb3523a8a412e618b092e5668d2961f",
+    "url": "/tale/index.html"
+  },
+  {
+    "revision": "696bb997c4a2a8eb7d1a",
+    "url": "/tale/static/css/main.476911b1.chunk.css"
+  },
+  {
+    "revision": "a0e195b03cdfde3418a4",
+    "url": "/tale/static/js/2.12a5bd00.chunk.js"
+  },
+  {
+    "revision": "696bb997c4a2a8eb7d1a",
+    "url": "/tale/static/js/main.abc3877c.chunk.js"
+  },
+  {
+    "revision": "d41bf600e105693fd3fa",
+    "url": "/tale/static/js/runtime~main.4bf973ca.js"
+  }
+]);
